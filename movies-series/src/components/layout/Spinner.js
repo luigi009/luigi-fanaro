420fc597800fc224ecf8af5b706fbdc9ea0c269f
luigi009 <luigifanaro@hotmail.com>
2021-03-17 14:28:00 -0300

    Update

--- movies-series/src/components/layout/Spinner.js
+++ movies-series/src/components/layout/Spinner.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import spinner from './spinner.gif'
 
 function Spinner() {
 
@@ -8,28 +7,22 @@ function Spinner() {
   for( var i = 0; i < 4; i++) {
     loadingCard.push(
       <div className="ssc col-md-3 mb-5">
-      <div className="ssc-wrapper ssc-card">
-        <div className="d-flex justify-content-center">
-          <div className="w-100">
-            <div className="ssc-square mb" style={{height: '200px'}}></div>
-            <div className="ssc-square mb" style={{height: '30px'}}></div>
-            <div className="ssc-square mb" style={{height: '35px'}}></div>
+        <div className="ssc-wrapper ssc-card">
+          <div className="d-flex justify-content-center">
+            <div className="w-100">
+              <div className="ssc-square mb" style={{height: '332px'}}></div>
+              <div className="ssc-square mb" style={{height: '30px'}}></div>
+              <div className="ssc-square mb" style={{height: '35px'}}></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     )
   }
 
   return (
     <>
-      {/* <img
-        src={spinner}
-        style={{ width: '200px', margin: 'auto', display: 'block' }}
-        alt="Loading..."
-      /> */}
-
-      <div className="d-flex flex-row">{loadingCard}</div>
+      <div className="loading-container">{loadingCard}</div>
     </>
   )
 }
